067a92a23528445ab837b6a1c1c52e37e747e351
Aryan Raj <aryanraj@Aryans-MacBook-Air-93.local>
2026-03-10 19:19:58 +0530

    second commit

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,4 +1,4 @@
 import { treaty } from "@elysiajs/eden";
 import type { App } from "../app/api/[[...slugs]]/route";
 
-export const client = treaty<App>("http://localhost:3000").api;
+export const client = treaty<App>("http://trashchat.aryannmn.dev").api;
